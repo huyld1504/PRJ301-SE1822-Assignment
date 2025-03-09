ce23445cc900777074ad68593d953f9d3aa0b1d1
ledinhhuy <ledinhhuy0105@gmail.com>
2025-03-09 21:23:48 +0700

    feat: service list ok

--- src/java/dao/ServiceDAO.java
+++ src/java/dao/ServiceDAO.java
@@ -19,26 +19,67 @@ import utils.DBUtils;
  */
 public class ServiceDAO {
 
-    public ArrayList<Service> getServiceByID() {
-        ArrayList<Service> result = new ArrayList<>();
+    public Service getServiceByID() {
+        Service result = null;
         Connection conn = null;
         PreparedStatement ps;
         ResultSet table;
-        
+
         try {
             conn = DBUtils.getConnection();
-            
-            if(conn != null) {
+
+            if (conn != null) {
                 String sql = "";
             }
         } catch (Exception e) {
-        } finally{
+        } finally {
             try {
-                if(conn != null) conn.close();
+                if (conn != null) {
+                    conn.close();
+                }
             } catch (SQLException e) {
             }
         }
 
         return result;
+    }
+
+    public ArrayList<Service> getAllServices() {
+        ArrayList<Service> list = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ps;
+        ResultSet table;
+
+        try {
+            conn = DBUtils.getConnection();
+
+            if (conn != null) {
+                String sql = "SELECT [serviceID]\n"
+                        + "      ,[serviceName]\n"
+                        + "      ,[hourlyRate]\n"
+                        + "  FROM [dbo].[Service]";
+                ps= conn.prepareStatement(sql);
+                table = ps.executeQuery();
+                
+                while(table.next()) {
+                    String serviceID = table.getString("serviceID");
+                    String serviceName = table.getString("serviceName");
+                    double hourlyRate = table.getDouble("hourlyRate");
+                    Service s = new Service(serviceID, serviceName, hourlyRate);
+                    list.add(s);
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
